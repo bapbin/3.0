ae00f64bf59d081c467f412731da01ec91edf03d
Bridget.Robbins <bridgetrobbins@gmail.com>
2017-01-25 07:39:31 +1300

    Reactstrapping

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,48 @@
 import React from 'react';
 
-/* STATELESS functional component: 
- 	Never updates or chnages, only passed props (remove this.ref for props
- 	Does not need to be Class extending React.  */
  	
 const Header = (props) => {
 	return (
-		<header className="App-header">
-			<h1>Weirdly</h1>
-			<h3 className="tagline"><span>{props.tagline}</span></h3>
-		</header>
+		<div>
+			<nav className="navbar navbar-light navbar-fixed-top">
+		        <a className="navbar-brand weirdly-logo-svg">
+            		<img src="https://app.weirdlyhub.com/img/logo.png" alt="Weirdly Logo" className="logo" width="97"/>
+	            </a>
+	            <h4>{props.tagline}</h4>
+		        <img className="pull-xs-right img-circle"
+		             src="https://app.weirdlyhub.com/img/logo.png"
+		             alt="Generic placeholder" width="40" height="40"/>
+
+		        <form className="pull-xs-right nav-search">
+		            <input type="text" className="form-control" placeholder="Search"/>
+		        </form>
+	          <script src="https://unpkg.com/reactstrap/dist/reactstrap.min.js">
+			        {console.log("cdn")}
+		      </script>
+			</nav>
+
+			<div className='container-fluid page-title'> 
+			    <div className="row">
+			        <div className="col-sm-8">
+			            <h1>quiz.title</h1>
+			            <h2>Location, City name</h2>
+			            <h2>Closes in <strong>5 days</strong> and <strong>4 hours</strong></h2>
+			        </div>
+
+			        <div className="col-sm-3 pull-xs-right page-title-nav">
+			            <a>Edit</a>
+			            <a>View</a>
+			            <a>Analytics</a>
+			        </div>
+		        </div>
+	        </div>
+
+		</div>
 	)
 }
+
+/* STATELESS functional component: 
+ 	Never updates or changes, only passed props (remove this.ref for props
+ 	Does not need to be Class extending React.  */
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
-import './v3.css';
-import './main.css';
+import './css/v3.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 
